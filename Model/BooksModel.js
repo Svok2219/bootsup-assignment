323ac662abac45acb02dc670cf768d6a1a7dcf37
shuvokoiri0@gmail.com <shuvokoiri0@gmail.com>
2023-04-30 23:36:49 +0600

    seventh

--- Model/BooksModel.js
+++ Model/BooksModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const BooksModel = mongoose.Schema({
   Name: { type: String, required: true },
+  picture: { type: String, required: true },
   Author: { type: String, required: true },
 });
 exports.Books = mongoose.model("Books", BooksModel);
--- Router/BooksRouter.js
+++ Router/BooksRouter.js
@@ -6,6 +6,7 @@ router.post("/", async (req, res) => {
   console.log(req.body);
   const BooksBody = new Books({
     Name: req.body.Name,
+    picture: req.body.picture,
     Author: req.body.BooksAuthor,
   });
   const PostedBooksBody = await BooksBody.save();
